41face5e2dfb1e3467e2d262bb7ae76eee5f6993
Noam Cohen-Ofir <109899445+noamco23-meet@users.noreply.github.com>
2022-12-06 21:47:55 +0200

    lab done first bonus done
    ngl this was an epic lab it was simpler than other ones but cool subject

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
+import json, requests
 app = Flask(__name__)
+
+headers = {'Authorization': 'Key 4496012e67ac46eeb75687504b4429d5'}
+api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
+people = {"people", "person", "adult", "teen", "child", "children", "baby", "babies" "man", "men", "women", "woman"}
+
 
 @app.route('/')
 def home():
@@ -7,16 +14,26 @@ def home():
 
 @app.route('/study_image', methods = ['POST'])
 def study_image():
-    
     image_url = request.form['url-input']
-    # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
-    # and read the result, make sure that you read and understand the
-    # example we covered in the slides! 
 
-    # YOUR CODE HERE!
+    data ={"inputs": [{"data": {
+          "image": {"url": image_url}}}]}
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    response_dict = json.loads(response.content)['outputs'][0]['data']['concepts']
+    results = []
+    for key in response_dict:
+        print(key)
+        results.append(key['name'])
     
-    return render_template('home.html', results="No results yet :(")
+    results_set = set(results)
+    human = results_set.intersection(people)
+    isHuman = False
+    if len(human) != 0:
+        isHuman = True
+    if "no person" in results_set:
+        isHuman = False
+    return render_template('home.html', results=results, length=len(results), human=isHuman)
 
 if __name__ == '__main__':
     app.run(debug=True)
